9385a184025a0098d1eb88771b7ea828e556cefa
arnyarny <ucab.arny1@gmail.com>
2022-12-21 20:23:36 +0800

    recent commit

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 var quotes = [
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    'ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    'dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    'sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    'amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    'consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    'adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    'elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    'do eiusmod tempor incididunt ut labore et dolore magna aliqua. u'
+    'Alles Gute zum Geburtstag',
+    'Viel Glück und Gesundheit',
+    'Du bist wunderbar',
+    'İyi ki doğdun',
+    'Happy birthday',
+    'Je te laisserai des mots',
+    'Better Call Kadriye',
+    'Du bist ein guter Freund',
+    'Enjoy your Geburtstag',
+    'Merhaba'
 ]
 
 function newQuote() {
     var randomNumber = Math.floor(Math.random() * (quotes.length));
     document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
 }
+// dots is an array of Dot objects,
+// mouse is an object used to track the X and Y position
+   // of the mouse, set with a mousemove event listener below
+   var dots = [],
+   mouse = {
+     x: 0,
+     y: 0
+   };
